5d85512eda204c323f613dbfe3ed98324d6d2557
Fauzy169 <ahmadfauzy169@gmail.com>
2024-12-19 17:25:36 +0800

    font Sora

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Preloader</title>
+    <link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/preloader.css') }}">
 </head>
 <body>
